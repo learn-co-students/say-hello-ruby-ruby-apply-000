094b894d16ce8e4159e008b566ebcc8bf8ffc25e
rational-flag-9665 <kelvinherreravidals@gmail.com>
2020-09-26 22:02:22 +0000

    Done.

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -8,7 +8,6 @@ describe "say_hello" do
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
   end
 end
